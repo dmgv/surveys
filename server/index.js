7d02d7be9bbead2405ec1e5b3f2e0a456327ea63
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-05-21 10:10:19 -0300

    Refactor passport to its own files

--- server/index.js
+++ server/index.js
@@ -1,30 +1,9 @@
 const express = require("express");
-const passport = require("passport");
-const googleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
 
 const app = express();
-passport.use(
-  new googleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleCleintSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("Accest Token: ", accessToken);
-      console.log("Refresh Token: ", refreshToken);
-      console.log("Profile: ", profile);
-    }
-  )
-);
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
