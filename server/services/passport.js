65e83c753519b383eaa3d5e484ff51eb82f96ec8
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-05-27 12:49:41 -0300

    Fix another typo

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,9 @@
 const passport = require("passport");
-const googleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
-const User = mongoose.model("User");
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -16,7 +16,7 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(
-  new googleStrategy(
+  new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
