8b38676324e2eae1e989c047b96e0f81db27b7cc
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-05-29 15:44:53 -0300

    Add MaterializeCSS

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from "react";
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>header</h1>
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">
+            Survey
+          </a>
+          <ul className="right">
+            <li>
+              <a href="#">Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,10 @@
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import App from "./components/App";
+
 // import reducers from './reducers';
 
 const store = createStore(() => [], {}, applyMiddleware);
